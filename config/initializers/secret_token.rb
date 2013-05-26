49d595bb626278d89a4dfd0a8d7dd485e9b9e596
Feather <lisafeather@gmail.com>
2013-05-26 11:32:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExquisiteCorpse::Application.config.secret_token = '662d328c9bdd94f1960185f49533be8442dd2950cd292d68a7cd885bc3afbece0fd1234b7664831cabfd58c68d2ab8b6b9579fc3fed5d14d63f16a63c4ef43d0'
